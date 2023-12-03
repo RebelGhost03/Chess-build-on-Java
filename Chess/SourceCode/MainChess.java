b096cfde34cbdead111bf02fc42e3f3a8e4d443a
RebelGhost03 <93103816+RebelGhost03@users.noreply.github.com>
2023-12-03 20:54:32 +0700

    Add files via upload

--- Chess/SourceCode/MainChess.java
+++ Chess/SourceCode/MainChess.java
@@ -1,16 +1,9 @@
 package SourceCode;
 
-import SourceCode.Engine.Board.Board;
 import SourceCode.GUI.Table;
 
 public class MainChess {
     public static void main(String[] args) {
-        
-        Board board = Board.createStandardBoard();
-
-        //System.out.println(board);
-
         Table table = new Table();
-
     }
 }
