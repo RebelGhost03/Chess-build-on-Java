07bc5e67312163d3f96ad715f9dc3ee3a660792e
RebelGhost03 <93103816+RebelGhost03@users.noreply.github.com>
2023-12-17 20:20:05 +0700

    Delete SourceCode directory

--- SourceCode/Engine/Player/AI/BoardEvaluation.java
+++
@@ -1,9 +0,0 @@
-package SourceCode.Engine.Player.AI;
-
-import SourceCode.Engine.Board.Board;
-
-public interface BoardEvaluation {
-    
-    int evaluate(Board board, int depth);
-
-}
--- SourceCode/MainChess.java
+++
@@ -1,9 +0,0 @@
-package SourceCode;
-
-import SourceCode.GUI.Table;
-
-public class MainChess {
-    public static void main(String[] args) {
-        Table.get().show();
-    }
-}
